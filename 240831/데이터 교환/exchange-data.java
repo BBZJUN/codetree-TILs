60aade0289eb2e901299c7123bb3aec0be141d80
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-08-31 15:52:10 +0900

    Title: 데이터 교환, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240831/데이터 교환/exchange-data.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 5;
+        int b = 6;
+        int c = 7;
+
+        int tmp;
+        tmp = a;
+        a= b;
+        b= c;
+        c = tmp;
+        
+        System.out.println(a);
+        
+        System.out.println(b);
+        
+        System.out.println(c);
+    }
+}
