0e8f6b4a29991d8bea7f2cfc2583e34e8d973cbe
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-01 17:33:58 +0900

    Title: 알파벳에 따른 평가, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240901/알파벳에 따른 평가/evaluation-by-alphabet.java
+++ 240901/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -5,19 +5,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         char a = sc.next().charAt(0);
 
-        if (a == "S") {
+        if (a == 'S') {
             System.out.println("Superior");
         }
-        else if (a == "A") {
+        else if (a == 'A') {
             System.out.println("Excellent");
         }
-        else if (a == "B"){
+        else if (a == 'B'){
             System.out.println("Good");
         }
-        else if (a == "C"){
+        else if (a == 'C'){
             System.out.println("Usually");
         }
-        else if (a == "D"){
+        else if (a == 'D'){
             System.out.println("Effort");
         }
         else{
