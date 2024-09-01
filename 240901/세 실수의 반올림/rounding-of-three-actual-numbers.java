7545fae79ed7f90979cd45f6527e0a479828f5c0
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-01 15:40:29 +0900

    Title: 세 실수의 반올림, Time: 148ms, Memory: 11MB, Status: Passed - CodeTree

--- 240901/세 실수의 반올림/rounding-of-three-actual-numbers.java
+++ 240901/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -6,8 +6,8 @@ public class Main {
         double a = sc.nextDouble();
         double b = sc.nextDouble();
         double c = sc.nextDouble();
-        System.out.printf("%.3f",a+0.0005);
-        System.out.printf("%.3f",b+0.0005);
-        System.out.printf("%.3f",c+0.0005);
+        System.out.printf("%.3f\n",(a));
+        System.out.printf("%.3f\n",(b));
+        System.out.printf("%.3f",(c));
     }
 }
