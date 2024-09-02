cbe765d1b12dc8049961a068f1cdb4c73d2fc745
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-02 19:13:49 +0900

    Title: 두 사람, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240902/두 사람/two-person.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt();
+        char a2 = sc.next().charAt(0);
+        int b1 = sc.nextInt();
+        char b2 = sc.next().charAt(0);
+
+        if ((a1 >=19 || b1>=19) &&(a2 =='M' || b2=='M')) {
+            System.out.println("1");
+        }
+        else {
+            System.out.println("0");
+        }
+    }
+}
