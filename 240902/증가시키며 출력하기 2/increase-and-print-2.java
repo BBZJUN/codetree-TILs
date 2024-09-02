34e4cf7c2f344c50e23a18a4d97feaa4b372082e
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-02 21:21:45 +0900

    Title: 증가시키며 출력하기 2, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

--- 240902/증가시키며 출력하기 2/increase-and-print-2.java
+++ 240902/증가시키며 출력하기 2/increase-and-print-2.java
@@ -5,7 +5,7 @@ public class Main {
         int i = 10;                  // 시작 값(초기화식)
 
         while (i <= 26) {           // 계속 반복할 조건(조건식)
-            System.out.println(i);  // 원하는 코드 작성
+            System.out.print(i+" ");  // 원하는 코드 작성
             i++;                    // 원하는 증감값 설정(증감식)
         }
             }
