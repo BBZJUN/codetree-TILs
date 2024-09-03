d26b00a63b9d39e0b1e0c343f80b67964b321d9c
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-03 19:30:49 +0900

    Title: 자연수면 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240903/자연수면 출력/output-only-natural-number.java
+++ 240903/자연수면 출력/output-only-natural-number.java
@@ -8,7 +8,7 @@ public class Main {
 
         if (a>0){
             for (int i = 1; i <=b; i++) {
-                System.out.print(i);
+                System.out.print(a);
         }
         else{
             System.out.print(0);
