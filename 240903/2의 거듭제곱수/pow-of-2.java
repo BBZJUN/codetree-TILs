231706804938df8d791d8e3b42306ff59a2ca574
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-03 21:33:11 +0900

    Title: 2의 거듭제곱수, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240903/2의 거듭제곱수/pow-of-2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c= 0;
+        while (true) {
+            n=n/2;
+            c++;
+            if(n==1){
+                break;
+            }
+            
+            
+        }
+        System.out.println(c);
+    }
+}
