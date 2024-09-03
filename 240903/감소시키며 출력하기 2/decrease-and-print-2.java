1c568c344eed0cab7fd9ca69fee6b6ab988d84b5
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-03 19:08:47 +0900

    Title: 감소시키며 출력하기 2, Time: 109ms, Memory: 9MB, Status: Passed - CodeTree

--- 240903/감소시키며 출력하기 2/decrease-and-print-2.java
+++ 240903/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -5,7 +5,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
 
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+
         int a=26;
         while(a>=10){
             System.out.print(a+" ");
