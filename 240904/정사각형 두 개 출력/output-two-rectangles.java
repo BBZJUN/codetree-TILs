e29f2829978e93b9741a1842f39a497aa4762232
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-04 15:26:34 +0900

    Title: 정사각형 두 개 출력, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240904/정사각형 두 개 출력/output-two-rectangles.java
+++ 240904/정사각형 두 개 출력/output-two-rectangles.java
@@ -7,7 +7,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
             
             for(int j = 0; j < n; j++) {
-                System.out.print("* ");
+                System.out.print("*");
                 
             }
             System.out.println();
@@ -16,7 +16,7 @@ public class Main {
          for (int i = 0; i < n; i++) {
             
             for(int j = 0; j < n; j++) {
-                System.out.print("* ");
+                System.out.print("*");
                 
             }
             System.out.println();
