e3ebab1537c025ae44698022f4b7be62c0a3540e
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-04 15:38:24 +0900

    Title: 별표 출력하기 4, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240904/별표 출력하기 4/print-star-4.java
+++ 240904/별표 출력하기 4/print-star-4.java
@@ -6,16 +6,17 @@ public class Main {
         int n = sc.nextInt();
         
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
 
-            for (int j = 0; j < n-i-1 ;j++){
+            for (int j = 0; j < n-i ;j++){
                 
                 System.out.print("* "); 
             }
             System.out.println();
         }
         for (int i = 0; i < n; i++) {
-
+            if(i==0)
+                continue;
             for (int j = 0; j < i+1 ;j++){
                 
                 System.out.print("* "); 
