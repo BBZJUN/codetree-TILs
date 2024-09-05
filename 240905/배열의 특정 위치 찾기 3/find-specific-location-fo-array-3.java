50dba015b82568128f2347f6e24fed718f2e1ca7
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-05 20:28:45 +0900

    Title: 배열의 특정 위치 찾기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240905/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
+++ 240905/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -15,6 +15,6 @@ public class Main {
             }
         }
 
-        System.out.println(arr[check - 1]+arr[check - 2]+arr[check - 3]);
+        System.out.println((arr[check - 1]+arr[check - 2]+arr[check - 3]));
     }
 }
