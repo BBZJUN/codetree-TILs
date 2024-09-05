b3ed254c113ea53f2a78bb95fd8b37ba08c675ec
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-05 20:14:00 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240905/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];  // 크기가 n인 실수형을 저장하기 위한 배열 선언
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i]%2==0){
+                System.out.println(arr[i]);
+            }           
+            
+        }
+
+        
+        
+    }
+}
