537e6a1f56dbe758f5792838876048fd34eae713
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-05 20:23:33 +0900

    Title: 배열의 특정 위치 찾기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240905/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+
+        int sum2 = 0;
+        int sum3=0;
+        for (int i = 0; i < 10; i += 1) {
+            arr[i] = sc.nextInt();
+            
+        }
+        for (int i = 0; i < 10; i += 2) {
+            sum2+=arr[i];
+            
+        }
+        for (int i = 0; i < 10; i += 3) {
+            sum3+=arr[i];
+            
+        }
+
+        System.out.printf("%d %.1f"sum2,(double)sum3/3);
+    }
+}
