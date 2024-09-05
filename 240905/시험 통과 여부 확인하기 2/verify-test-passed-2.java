659bd03aa27d203c2bba668a37e406da110bf9f4
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-05 20:10:20 +0900

    Title: 시험 통과 여부 확인하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240905/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[10];  // 크기가 n인 실수형을 저장하기 위한 배열 선언
+
+        int sum=0;
+        int c=0;
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+            arr[i] = a+b+c+d;
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i]/4>=0){
+                c++;
+                System.out.println("pass");
+            }
+            else{
+                System.out.println("fail");
+            }
+            
+            
+        }
+
+        System.out.printf("%d", c);
+        
+        
+    }
+}
