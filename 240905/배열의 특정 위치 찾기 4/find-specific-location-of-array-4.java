fdc212cedd720ba23ce334c628d031f786f529f4
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-05 20:05:53 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240905/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];  // 크기가 n인 실수형을 저장하기 위한 배열 선언
+
+        int sum=0;
+        int c=0;
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+
+            if (arr[i]==0){
+                break;
+            }
+            if (arr[i]%2==0){
+                sum+=arr[i];
+                c++; 
+            }
+            
+            
+        }
+
+        System.out.printf("%d %d", c,sum);
+        
+        
+    }
+}
