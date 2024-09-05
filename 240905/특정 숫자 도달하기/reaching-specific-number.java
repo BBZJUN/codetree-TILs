340ebb0659342f1f731ee698d24f24b6a40c93c6
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-05 19:48:38 +0900

    Title: 특정 숫자 도달하기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240905/특정 숫자 도달하기/reaching-specific-number.java
+++ 240905/특정 숫자 도달하기/reaching-specific-number.java
@@ -3,19 +3,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] x = new int[10];
+
+        // 변수 선언
+        int[] arr = new int[10];
         int sum = 0;
-        int c = 0;
-        for (int i = 0; i < 10; i++) {
-            int val = sc.nextInt();
-            if (val >= 250){
-                x[i]=val;
-                sum+=val;
-                c++;
-            }
-            
-        }
-        System.out.printf("%d %.1f",sum,(double)sum/c);
+        int cnt = 0;
         
+        // 10개의 정수를 입력받습니다.
+        for(int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // 10개의 정수 중 250 이상의 수가 나올 때 까지의 수의 합계와 개수를 구합니다.
+        for(int i = 0; i < 10; i++) {
+            if(arr[i] >= 250) 
+                break;
+            sum += arr[i];
+            cnt++;
+        }
+        
+        // 평균을 구합니다.
+        double avg = (double)sum / cnt;
+
+        // 출력
+        System.out.printf("%d %.1f", sum, avg);
     }
 }
