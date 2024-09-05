60f1c6d0070b6dbf8526423d1fdfe871dccf6f56
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-05 19:57:46 +0900

    Title: 배열 채우기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240905/배열 채우기/filling-array.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];  // 크기가 n인 실수형을 저장하기 위한 배열 선언
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i]==0){
+                break;
+            }
+        }
+        for (int k=i-1;k>=0;k--){
+            System.out.print(arr[k]+" ");
+        }
+        
+    }
+}
