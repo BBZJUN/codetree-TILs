d2213f05d8bf2f3693e226578fac7a0bbb527132
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-05 19:18:04 +0900

    Title: 별표 출력하기 8, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240905/별표 출력하기 8/print-star-8.java
+++ 240905/별표 출력하기 8/print-star-8.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 0; i < 6; i++) {
+        for (int i = 1; i <= 6; i++) {
             if (i % 2 == 0) {
-                for (int j = 0; j < i + 1; j++) {
-                    System.out.print("*");
+                for (int j = 0; j < i; j++) {
+                    System.out.print("* ");
                 }
             }
             else {
