286e5efc0ce50262e40c6999581de5d55be181e1
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-05 19:53:45 +0900

    Title: 학점 계산기, Time: 153ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240905/학점 계산기/credit-calculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        double[] arr = new double[n];  // 크기가 n인 실수형을 저장하기 위한 배열 선언
+        double sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextDouble();
+            sum += arr[i];
+        }
+        double avg = sum/n;
+        if (sum>=4.0){
+            System.out.println("Perfect");
+        }
+        else if (sum>=3.0){
+            System.out.println("Good");
+        }
+        else{
+            System.out.println("Poor");
+        }
+        
+    }
+}
