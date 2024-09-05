206e5aa8347c2b306f8894030df3129e657c6e97
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-05 19:13:44 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240905/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
+++ 240905/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -7,21 +7,18 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 0; i<n;i++){
-            for (int j=n-i;j>0;j--){
-                System.out.print(" ");
+            for (int j=n-i;j>1;j--){
+                System.out.print("  ");
             }
             for (int j=i; j>=0;j--){
-                System.out.print("@");
+                System.out.print("@ ");
             }
             System.out.println();
         }
 
-        for (int i = n-2; i>=0;i--){
-            for (int j=n-i;j>0;j--){
-                System.out.print("@");
-            }
-            for (int j=i; j>=0;j--){
-                System.out.print(" ");
+        for (int i = n-1; i>0;i--){
+            for (int j=i;j>0;j--){
+                System.out.print("@ ");
             }
             System.out.println();
         }
