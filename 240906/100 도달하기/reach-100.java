abcaa33331a84a1e0b7b40471709ab36a1fad30d
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-06 17:30:40 +0900

    Title: 100 도달하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240906/100 도달하기/reach-100.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+        arr[0] = 1;
+        arr[1] = sc.nextInt();
+        int c = 0;
+        for (int i = 2; i<100; i++){
+            arr[i] = arr[i-1]+arr[i-2];
+            if(arr[i]>100){
+                c=i;
+                break
+            }
+        }
+        for (int i=0;i<=c;i++){
+            System.out.print((arr[i])+" ");
+           
+
+        }
+
+    }
+}
