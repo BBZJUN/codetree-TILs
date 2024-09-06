1ff68a398d69ebb7c1f07f658fa738a55e4baa4e
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-06 17:03:11 +0900

    Title: 짝수인 것만 출력하기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240906/짝수인 것만 출력하기/print-even-number.java
+++ 240906/짝수인 것만 출력하기/print-even-number.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i<n; i++){
-            arr[i]=sc.nextInt()l;
+            arr[i]=sc.nextInt();
         }
 
 
