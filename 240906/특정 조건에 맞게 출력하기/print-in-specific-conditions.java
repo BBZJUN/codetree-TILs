10d05c69ff0ac1767a54458f0e701fb318bdf179
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-06 17:21:36 +0900

    Title: 특정 조건에 맞게 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240906/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+        int ch=0;
+        for (int i = 0; i<100; i++){
+            arr[i] = sc.nextInt();
+            if (arr[i]==0){
+                ch = i;
+            }
+        }
+        for (int i=0;i<ch;i++){
+            if(arr[i]%2==0){
+                System.out.print((arr[i]/2)+" ");
+            }
+            else{
+                System.out.print((arr[i]+3)+" ");
+            }
+
+        }
+
+    }
+}
