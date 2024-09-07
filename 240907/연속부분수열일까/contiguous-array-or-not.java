d9eed72ed54d41e14dfb7f428feef25e6cab1005
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-07 14:23:52 +0900

    Title: 연속부분수열일까, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240907/연속부분수열일까/contiguous-array-or-not.java
+++ 240907/연속부분수열일까/contiguous-array-or-not.java
@@ -22,7 +22,7 @@ public class Main {
 
         for (int i = 0 ;i<n1;i++){
             if (B[0]==A[i]){
-                for (int j = 0 ; j<n2;j++){
+                for (int j = 0 ; j<n2 && (i+j)<n1;j++){
                     if (B[j]==A[i+j] && (i+j)<n1){
                         if (j==n2-1){
                             System.out.print("Yes");
