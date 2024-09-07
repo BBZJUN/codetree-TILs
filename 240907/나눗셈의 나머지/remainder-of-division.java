78aa94b1d5a32cc473847053ad5392c453da2c68
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-07 13:16:20 +0900

    Title: 나눗셈의 나머지, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240907/나눗셈의 나머지/remainder-of-division.java
+++ 240907/나눗셈의 나머지/remainder-of-division.java
@@ -3,32 +3,42 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[100];
-        int[] r = new int[100];
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int i = 0;
-        int x = 1;
-        int sum = 0;
-
-        for( ; ;i++){
+        int[] r = new int[b];
+        int check = 0;
+        int sum=0;
+        int x= 0;
+        for (int i = 0; ;i++){
+            r[a%b]++;
+            a=a/b;
             if (a<=1){
+                check = i;
                 break;
             }
-            arr[i]=a/b;
-            r[i]=a%b;
-            a=arr[i];
         }
-        for (int k=0;k<i;k++){            
-            x = 1;
-            for (int j=0;j<r[k];j++){
-                x=x*2;
+        for (int i=0;i<check;i++){
+            if(i==0){
+                if(r[0]>=1){
+                    sum=sum+1;
+                }
             }
-            System.out.println(x);
-            sum=sum+x;         
-            
+            else{
+                if(r[i]!=0)
+                {   
+                    x=1;
+                    for (int j=0;j<r[i];j++){
+                        x=x*2;
+                    }
+                }
+                else{
+                    x=0;
+                }
+                sum=sum+x;
+                
+            }
         }
         System.out.print(sum);
         
-}
+    }
 }
