54241cc36ce1c15b90351218505e105ee46086f9
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-07 14:26:10 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 182ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240907/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int[] A = new int[n];
+        int c= 0;
+        for(int i = 0 ;i<n;i++){
+            A[i]=sc.nextInt();
+        }
+        for(int i = 0 ;i<n;i++){
+            if (A[i]==2){
+                c++;
+                if (c==3){
+                    System.out.print(i+1);
+                }
+            }
+        }
+
+
+
+        
+    }
+}
