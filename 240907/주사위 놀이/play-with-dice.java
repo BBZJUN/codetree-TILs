a60ac208509e0c1f5ad27c7fb999c5ca5de0d7e0
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-07 12:27:43 +0900

    Title: 주사위 놀이, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240907/주사위 놀이/play-with-dice.java
+++ 240907/주사위 놀이/play-with-dice.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
         int[] arr = new int[10];
         int[] countArr = new int[7];
 
