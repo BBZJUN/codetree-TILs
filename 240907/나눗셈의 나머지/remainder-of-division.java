9c1de2b452481e6b557b0cf377d04935655b16a6
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-07 13:24:39 +0900

    Title: 나눗셈의 나머지, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240907/나눗셈의 나머지/remainder-of-division.java
+++ 240907/나눗셈의 나머지/remainder-of-division.java
@@ -3,26 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // 입력 받기
         int a = sc.nextInt();
         int b = sc.nextInt();
         
-        // 나머지의 등장 횟수를 저장할 배열
-        int[] r = new int[b];
+        // 나머지가 등장한 횟수를 기록할 배열
+        int[] remainderCount = new int[b];
         
         // 나눗셈 반복
-        while (a > 0) {
-            int remainder = a % b;
-            r[remainder]++; // 나머지 등장 횟수 증가
-            a = a / b; // 몫을 다시 a로 저장
+        while (a > 1) {
+            int remainder = a % b;  // 나머지 계산
+            remainderCount[remainder]++;  // 나머지 등장 횟수 증가
+            a = a / b;  // 몫을 a에 저장
         }
         
-        // 나머지 등장 횟수의 제곱의 합을 계산
+        // 나머지 등장 횟수의 제곱의 합 계산
         int sum = 0;
         for (int i = 0; i < b; i++) {
-            sum += r[i] * r[i]; // 각 나머지의 등장 횟수 제곱의 합
+            sum += remainderCount[i] * remainderCount[i];  // 각 나머지 등장 횟수 제곱
         }
         
         // 결과 출력
         System.out.println(sum);
+        
+        sc.close();
     }
 }
