84215845c93ea59858f33d81304adb2630b12aee
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-10 11:32:56 +0900

    Title: 두 숫자의 차의 최솟값, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240910/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
+++ 240910/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
@@ -16,7 +16,7 @@ public class Main {
         int r = 999;
         for (int i=n-1; i>=0 ;i--){
             for (int j=0; j<i; j++){
-                if ((x[i]-x[j]) > r){
+                if ((x[i]-x[j]) < r){
                     r = x[j]-x[i];
                 }
             }
