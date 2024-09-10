98fa08ebbed783b5aa3fac3648226be329954dc1
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-10 11:25:18 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240910/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 240910/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -6,6 +6,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] x = new int[n];
+
+
+        for (int i=0; i<n ;i++){
+            x[i] = sc.nextInt();
+        }
+
+
         int r = -1;
         for (int i=0; i<n-1 ;i++){
             for (int j=i+1; j<n; j++){
