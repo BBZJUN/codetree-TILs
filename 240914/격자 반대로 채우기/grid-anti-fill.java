7055f62e94bf03e0f0e585be76ad64851251d359
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-14 00:29:50 +0900

    Title: 격자 반대로 채우기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240914/격자 반대로 채우기/grid-anti-fill.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int check = 1;
+        int num= 1;
+        for (int i = n-1; i>=0; i--){
+
+            if (check==0)
+            {
+                for (int j = 0 ; j<n;j++){
+                    x[i][j]=num++;
+                }
+                check = 1;
+            }
+
+            else
+            {
+                for (int j=n-1; j>=0; j--){
+                    x[i][j]=num++;
+                }
+                check = 0;
+            }
+        }
+
+        for (int i = 0 ; i<n;i++){
+            for (int j = 0 ; j<n;j++){
+                System.out.print(x[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
