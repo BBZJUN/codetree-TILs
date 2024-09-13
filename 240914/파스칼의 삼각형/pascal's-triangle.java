bf4395e34c48145668396f11f8d0ff84bbef84a4
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-14 00:43:59 +0900

    Title: 파스칼의 삼각형, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240914/파스칼의 삼각형/pascal's-triangle.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] x = new int[n][n]; 
+
+        for (int i = 0; i<n;i++){
+            for (int j = 0; j<n;j++){
+                if (j==0){
+                    x[i][j] = 1;
+                }
+                if(i==j){
+                    x[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 1; i<n;i++){
+            for (int j = 0; j<i;j++){
+                x[i][j] = x[i-1][j-1] + x[i-1][j];
+            }
+        }
+
+        // 출력
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(x[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
