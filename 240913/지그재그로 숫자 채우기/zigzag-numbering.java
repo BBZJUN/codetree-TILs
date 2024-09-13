966e793a38c6142f74277299af5939ffa245f9d0
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-13 23:44:20 +0900

    Title: 지그재그로 숫자 채우기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240913/지그재그로 숫자 채우기/zigzag-numbering.java
+++ 240913/지그재그로 숫자 채우기/zigzag-numbering.java
@@ -27,7 +27,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
             
                 for (int j = 0; j < m; j++){
-                    System.out.print(x[j][i]+" ");
+                    System.out.print(x[i][j]+" ");
                 }
             System.out.println();
             
