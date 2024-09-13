454f15c4f02880457313b7fccf9a0c200e40d0eb
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-14 00:55:07 +0900

    Title: 격자에 점 그리기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240914/격자에 점 그리기/draw-points-on-grid.java
+++ 240914/격자에 점 그리기/draw-points-on-grid.java
@@ -12,13 +12,13 @@ public class Main {
         for (int i = 0; i<m;i++){
             int x = sc.nextInt();
             int y = sc.nextInt();
-            arr[x][y] = num++;
+            arr[x-1][y-1] = num++;
         }
 
         // 출력
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(x[i][j] + " ");
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
             
