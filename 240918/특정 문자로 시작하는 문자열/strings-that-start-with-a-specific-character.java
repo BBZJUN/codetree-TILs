c2150ece419b253521167deae01b14eac8a83c6d
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-18 21:46:24 +0900

    Title: 특정 문자로 시작하는 문자열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240918/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        char x;
+        int c = 0 ;
+        int sum = 0;
+        for (int i=0; i<n;i++){
+            arr[i] = sc.next();
+            
+        }
+        x = sc.next().charAt(0);
+
+
+        for (int i=0; i<n;i++){
+            if (arr[i].charAt(0)==x){
+                c++;
+                sum=sum+arr[i].length();
+            }
+            
+            
+        }
+        System.out.printf("%d %.2f",c,(double)sum/c)
+        
+    }
+}
