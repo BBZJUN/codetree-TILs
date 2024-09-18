d6ccc98a2fe84fa05f97ce2455123b418532f5fc
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-18 22:09:03 +0900

    Title: Run Length 인코딩, Time: 107ms, Memory: 10MB, Status: Passed - CodeTree

--- 240918/Run Length 인코딩/run-length-encoding.java
+++ 240918/Run Length 인코딩/run-length-encoding.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] str = sc.next();
+        String str = sc.next();
         String result = new String();
         for (int i=0; i<str.length(); i++){
             int c = 0;
-            if (i==0 || i>0&&str[i]!=str[i-1]){
+            if (i==0 || i>0&&str.charAt(i)!=str.charAt(i-1)){
                 for (int j=i; j<str.length() ; j++){
-                    if (str[i]==str[j]){
+                    if (str.charAt(i)==str.charAt(j)){
                         c++;
                     }
+                    else{
+                        break;
+                    }
                 }
-                result = result + str[i] + c;
+                result = result + str.charAt(i) + c;
             }
         }
+        System.out.println(result.length());
         System.out.print(result);
         // 여기에 코드를 작성해주세요.
     }
