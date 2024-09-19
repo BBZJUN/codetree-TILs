8cc6f4400436b7611df3d8c2a6fc7244adf28505
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-19 21:16:09 +0900

    Title: 문자열 거꾸로 출력하기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240919/문자열 거꾸로 출력하기/print-string-backward.java
+++ 240919/문자열 거꾸로 출력하기/print-string-backward.java
@@ -7,12 +7,13 @@ public class Main {
 
         for (int i=0; i<10; i++){
             String A = sc.next();
-            for (int j=A.length()-1; j>=0;j--){
-                System.out.print(A.charAt(j));
-            }
             if(A.compareTo("END")==0){
                 break;
             }
+            for (int j=A.length()-1; j>=0;j--){
+                System.out.print(A.charAt(j));
+            }
+            
             System.out.println();
         }
 
