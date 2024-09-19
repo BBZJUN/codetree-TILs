87cd39889b497a9b5e82c7eb7b9c49d172f01930
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-19 21:14:57 +0900

    Title: 문자열 거꾸로 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240919/문자열 거꾸로 출력하기/print-string-backward.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+        for (int i=0; i<10; i++){
+            String A = sc.next();
+            for (int j=A.length-1; j>=0;j--){
+                System.out.print(A.charAt(j));
+            }
+            if(A.compareTo("END")==0){
+                break;
+            }
+            System.out.println();
+        }
+
+        
+    }
+}
