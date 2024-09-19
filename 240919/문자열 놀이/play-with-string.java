c48578b16e10ea39fd575871e24617c9969a8208
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-19 19:16:02 +0900

    Title: 문자열 놀이, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240919/문자열 놀이/play-with-string.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        int q = sc.nextInt();
+        char[] arr = s.toCharArray();
+        for (int i=0 ; i<q; i++){
+            int n = sc.nextInt();
+            if (n==1){
+                int A = sc.nextInt();
+                int B = sc.nextInt();
+                
+                char tmp = arr[A];
+                arr[A] = arr[B];
+                arr[B] = tmp;
+                String result = String.valueOf(arr);
+            }
+            else if (n==2){
+                char a = sc.next().charAt(0);
+                char b = sc.next().charAt(0);
+
+                for (int k=0; k<s.length();k++){
+                    if(arr[k]==a){
+                        arr[k]=b;
+                    }
+                    
+                }
+                String result = String.valueOf(arr);
+            }
+            System.out.println(result);
+        }
+    }
+}
