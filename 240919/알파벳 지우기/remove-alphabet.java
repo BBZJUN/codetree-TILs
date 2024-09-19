ddfe539581e259d6e5502fbe407d90637b71ea6b
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-19 20:52:36 +0900

    Title: 알파벳 지우기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240919/알파벳 지우기/remove-alphabet.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        String aa ="";
+        String bb = "";
+        int tmp = 0;
+        for (int i=0; i<a.length(); i++){
+            if (a.charAt(i)>='0' &&a.charAt(i)<='9'){
+                tmp = 1;
+                aa+=a.charAt(i);
+            }
+        }
+
+        tmp = 0;
+
+        for (int i=0; i<b.length(); i++){
+            if (b.charAt(i)>='0' && b.charAt(i)<='9'){
+                tmp = 1;
+                bb+=b.charAt(i);
+            }
+        }
+
+        System.out.print(Integer.parseInt(aa)+Integer.parseInt(bb));
+        
+    }
+}
