9e5f4f4ef87654870443960ade36497234b4014d
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-19 19:28:27 +0900

    Title: 문자 제거하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240919/문자 제거하기/del-char.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        for ( ; ; ){
+            if (s.length()==1){
+                break;
+            }
+            int x = sc.nextInt();
+            if(x>=s.length()-1){
+                s=s.substring(0,s.length()-2);
+            }
+            else{
+                s=s.substring(0,x)+s.substring(x+1,s.length());
+            }
+            SYstem.out.println(s);
+        }
+    }
+}
