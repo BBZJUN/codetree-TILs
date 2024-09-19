c33ffb501e627a209239b5adb104fa0876663c0a
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-19 20:12:31 +0900

    Title: 규칙에 따라 밀기, Time: 109ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240919/규칙에 따라 밀기/push-by-the-rules.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String q = sc.next();
+        char[] arr = q.toCharArray();
+        for (int i=0 ;i<q.length(); i++){
+            if (arr[i]=='L'){
+                s= s.substring(1,s.length())+s.substring(0,1);
+            }
+            else if(arr[i]=='R'){
+                s= s.substring(s.length() - 1,s.length())+s.substring(1,s.length()-1);
+            }
+        }
+        System.out.print(s);
+    }
+}
