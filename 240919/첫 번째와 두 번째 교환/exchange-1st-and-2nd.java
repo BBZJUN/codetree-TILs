652d007c2567201b5d9a2dc6526dbbb30212100e
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-19 18:39:44 +0900

    Title: 첫 번째와 두 번째 교환, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240919/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
+++ 240919/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
@@ -5,14 +5,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String s = sc.next();
-        char check1 = sc.charAt(0);
-        char check2 = sc.charAt(1);
+        char check1 = s.charAt(0);
+        char check2 = s.charAt(1);
         for (int i = 0; i<s.length(); i++){
-            if (sc.charAt(i)==check1){
-                
+            if (s.charAt(i)==check1){
+                s.charAt(i) = check2;
+            }
+            else if (s.charAt(i)==check2){
+                s.charAt(i) = check1;
             }
         }
 
-        System.out.print(s1.substring(0,2)+s2.substring(2,len));
+        System.out.print(s);
     }
 }
