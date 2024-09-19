e42138b8d2f8b9d1ce7b0fc1f324683065b9cead
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-19 20:01:52 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240919/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int q = sc.nextInt();
+        System.out.println(s);
+        for (int i=0 ;i<q; i++){
+            int n = sc.nextInt();
+            if (n==1){
+                s=s.substring(1,s.length())+s.substring(0,1);
+            }
+            else if(n==2){
+                s=s.substring(s.length()-1,s.length())+s.substring(0,s.length()-1);
+            }
+            else if(n==3){
+                String tmp ="";
+                for (int k=s.length(); k>0 ;k--){
+                    tmp=tmp+s.substring(k-1,k);
+                }
+            }
+            
+            System.out.println(s);
+        }
+    }
+}
