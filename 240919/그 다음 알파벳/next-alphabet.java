c5e41a80cdc6a9140f1347fdba1c83fb0fb2268c
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-19 20:25:54 +0900

    Title: 그 다음 알파벳, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240919/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        char next;
+        if (a=='z'){
+            next='a';
+        }
+        else{
+            char next = (char)(a+1);
+        }
+        
+        System.out.print(next);
+        
+    }
+}
