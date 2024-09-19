9895df2bda2ece484b7a6b159f4303ba98355bd9
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-19 20:27:22 +0900

    Title: 그 전 알파벳, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240919/그 전 알파벳/before-the-alphabet.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        char pre;
+        if (a=='a'){
+            pre='z';
+        }
+        else{
+            pre = (char)(a-1);
+        }
+        
+        System.out.print(next);
+        
+    }
+}
