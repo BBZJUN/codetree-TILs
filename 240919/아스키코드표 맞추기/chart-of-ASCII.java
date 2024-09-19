386e7a5f99070e908c9eaf36bfa37f6ebd8676f7
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-19 20:20:12 +0900

    Title: 아스키코드표 맞추기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240919/아스키코드표 맞추기/chart-of-ASCII.java
+++ 240919/아스키코드표 맞추기/chart-of-ASCII.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char[] arr = new char[5];
+        int[] arr = new int[5];
         for (int i=0; i<5;i++){
-            arr[i]=sc.next().charAt(0);
+            arr[i]=sc.nextInt();
         }
         for (int i=0; i<5;i++){
             System.out.print((char)arr[i]+" ");
