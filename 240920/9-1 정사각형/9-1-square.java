0be8d92dbd49c99f49a9e73396766e685ec51c54
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-20 14:03:42 +0900

    Title: 9-1 정사각형, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240920/9-1 정사각형/9-1-square.java
+++ 240920/9-1 정사각형/9-1-square.java
@@ -5,12 +5,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         // 여기에 코드를 작성해주세요.
         int n = sc.nextInt();
+        int g = 9;
         for (int i=0 ;i<n;i++){
-            if (n==0){
-                n=9;
+            if (g==0){
+                g=9;
             }
             for (int j=0;j<n;j++){
-                System.out.print(n--);
+                System.out.print(g--);
             }
             System.out.println();
         }
