24f362e3f56af969ff52f8ed9dd06afce0ca5a3d
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-22 15:51:45 +0900

    Title: 함수를 이용한 369 게임, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240922/함수를 이용한 369 게임/369-games-using-functions.java
+++ 240922/함수를 이용한 369 게임/369-games-using-functions.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void main(String[] args) {
+        // 변수 선언 및 입력:
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int cnt = 0;
+        for(int i = a; i <= b; i++)
+            if(is369Number(i))
+                cnt++;
+
+        System.out.println(cnt);
+    }
     // 3, 6, 9를 포함하거나 3의 배수인지를 판단합니다.
     public static boolean is369Number(int n) {
         return contains369(n) || (n % 3 == 0);
@@ -22,17 +35,5 @@ public class Main {
     }
 
     
-    public static void main(String[] args) {
-        // 변수 선언 및 입력:
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        int cnt = 0;
-        for(int i = a; i <= b; i++)
-            if(is369Number(i))
-                cnt++;
-
-        System.out.println(cnt);
-    }
+    
 }
