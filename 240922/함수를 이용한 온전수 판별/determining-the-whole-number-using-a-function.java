7d8c0b897c39131f2ab7f40c2f9774d881ba307a
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-22 16:11:36 +0900

    Title: 함수를 이용한 온전수 판별, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240922/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {    
+    // 온전수인지 아닌지 여부를 판단하는 함수를 작성합니다.
+    public static boolean judgeNumber(int n) {
+        if(n % 2 == 0)
+            return false;
+        else if(n % 10 == 5)
+            return false;
+        else if(n % 3 == 0 && n % 9 != 0)
+            return false;
+        else
+            return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력:
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int cnt = 0;
+
+        for(int i = a; i <= b; i++) {
+            if(judgeNumber(i))
+                cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+}
