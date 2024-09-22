23b6e44d753061c352b5cf0ef28d38abd5c11e06
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-22 15:34:45 +0900

    Title: 최소공배수 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240922/최소공배수 구하기/find-the-least-common-multiple.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int m = sc.nextInt();
+        f(n,m);
+    }
+
+
+    public static void f(int n, int m){
+        int re = n*m;
+        for (int i=0; i<=Math.min(n,m), i++){
+            if (n%i==0 && m%i==0){
+                re = i*(n/i)*(m/i);
+            }
+        }
+        System.out.print(re);
+    }
+}
