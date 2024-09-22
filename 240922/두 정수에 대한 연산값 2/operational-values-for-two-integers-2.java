4d456f74e35da1698a47fba74ea56778141ec406
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-22 19:34:20 +0900

    Title: 두 정수에 대한 연산값 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240922/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class IntW{
+    int val;
+
+    public IntW(int val){
+        this.val = val;
+    }
+}
+
+public class Main {
+
+    public static void f(IntW a, IntW b){
+        
+        if (a.val > b.val){
+            a.val*=2;
+            b.val+=10;
+        }
+        else{
+            b.val*=2;
+            a.val+=10;
+        }
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+        int w = sc.nextInt();
+        IntW a = new IntW(q);
+        IntW b = new IntW(w);
+        f(a,b);
+        System.out.print(a+" "+b);
+        
+    }
+}
