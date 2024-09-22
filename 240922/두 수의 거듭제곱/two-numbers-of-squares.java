8979176a64b62005a071e62cb328536c70279389
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-22 16:05:34 +0900

    Title: 두 수의 거듭제곱, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240922/두 수의 거듭제곱/two-numbers-of-squares.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {    
+    // a^b의 값을 반환합니다.
+    public static int power(int a, int b) {
+        int cnt = 1;
+        for(int i = 1; i <= b; i++)
+            cnt *= a;
+    
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력:
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(power(a, b));
+        
+    }
+}
