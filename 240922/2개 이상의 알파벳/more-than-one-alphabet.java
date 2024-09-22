2a8d914ca1a72f12176226c3314fcbc11f68f510
강재준 <90443794+BBZJUN@users.noreply.github.com>
2024-09-22 19:29:00 +0900

    Title: 2개 이상의 알파벳, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240922/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {    
+    // 문자열에 알파벳이 2개 이상인지 여부를 반환합니다.
+    public static boolean isMoreTwoalp(String str) {
+        // 하나라도 서로 다른 알파벳이 있다면
+        // 그 문자열은 알파벳이 2개 이상입니다.
+        int len = str.length();
+        for(int i = 0; i < len; i++)
+            if(str.charAt(i) != str.charAt(0))
+                return true;
+    
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력:
+        String A = sc.next();
+
+        if(isMoreTwoalp(A))
+            System.out.print("Yes");
+        else
+            System.out.print("No");
+    }
+}
